656f0a827ae8c2838d10c2d29d18bbfb96bebe61
RebeccaMulvaney <34610104+RebeccaMulvaney@users.noreply.github.com>
2018-05-21 11:27:00 +0200

    The about file
    This is the page where to user has an about the system.

---
+++ about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="formsStylesheet.css" />
+</head>
+<body style="text-align:center;">
+    <h1>About</h1>
+    <p>This program is a safe and friendly environment </p>
+    <p>for students to communicate with each other.</p>
+    <button onclick="goback()" id="submitButton">Back</p>
+
+    <script>
+        function goback(){
+            location.href = 'chatroom.php';
+        }
+    </script>
+</body>
+</html>
